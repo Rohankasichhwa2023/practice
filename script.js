dee2cef99843d87aad7dfee8219d214d37d05ae0
Rohan Kasichhwa <rohankasichhwa@gmail.com>
2025-09-03 19:44:57 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const storedUser = "admin";
+const storedHash = "9b0eb22aef89516d6fb4b31ccf008a68abe0d10a3fc606316389613eccf96854";
+
+async function sha256(message) {
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+}
+
+async function login() {
+    const user = document.getElementById("username").value.trim();
+    const pass = document.getElementById("password").value;
+
+    const inputHash = await sha256(pass);
+
+    if (user === storedUser && inputHash === storedHash) {
+        document.getElementById("result").innerText = "✅ Access Granted! Redirecting...";
+        setTimeout(() => {
+            window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1"; 
+        }, 2000);
+    } else {
+        document.getElementById("result").innerText = "❌ Wrong Try Again!";
+    }
+}
